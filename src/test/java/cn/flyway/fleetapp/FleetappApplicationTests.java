6538c7d9687e4896319a371ffa1152d88237f105
FlyHit <2692217528@qq.com>
2021-07-21 10:18:06 +0800

    Initial commit

---
+++ src/test/java/cn/flyway/fleetapp/FleetappApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.flyway.fleetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
